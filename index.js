fd072524cb9ec89d8acfe61d953a0272ce18c70c
Sheffin <sheff.tech11@gmail.com>
2024-07-06 00:59:33 +0530

    gpt fix

--- index.js
+++ index.js
@@ -129,10 +129,9 @@ app.post("/webhook", async (req, res) => {
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
     ) {
-      let phon_no_id =
-        body_param.entry[0].changes[0].value.metadata.phone_number_id;
+      let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      let msg_body = body_param.entry[0].changes[0].value.messages[0].text ? body_param.entry[0].changes[0].value.messages[0].text.body : "";
 
       console.log("Phone number: " + phon_no_id);
       console.log("From: " + from);
