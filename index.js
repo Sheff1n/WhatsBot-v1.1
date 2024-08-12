5ddb19e90beadec86ba1244e2231d95d537796be
Sheffin <sheff.tech11@gmail.com>
2024-08-12 15:48:29 +0530

    call button bug fix

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ const sendTemplateMessage = async (phone_number_id, to, access_token) => {
               type: "button",
               sub_type: "url",
               index: 1,
-              parameters: [], 
+              // parameters: [], 
               // phone_number: "9895260915", 
             },
           ],
