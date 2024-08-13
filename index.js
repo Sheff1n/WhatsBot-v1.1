3172072833fbd4675ee49bc75506eaa09d289ee4
Sheffin <sheff.tech11@gmail.com>
2024-08-13 15:26:14 +0530

    removed parameters

--- index.js
+++ index.js
@@ -205,12 +205,7 @@ const sendTemplateMessage = async (phone_number_id, to, access_token) => {
               type: "button",
               sub_type: "url",
               index:0,
-              parameters: [
-                {
-                  type: "text",
-                  text: "https://sheffin.online/",
-                },
-              ],
+              parameters: [],
             },
           ],
         },
