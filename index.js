59367efb6a90f849e6ac7d005ba0ec811a39bfbe
Sheffin <sheff.tech11@gmail.com>
2024-08-13 15:44:13 +0530

    try2

--- index.js
+++ index.js
@@ -205,7 +205,6 @@ const sendTemplateMessage = async (phone_number_id, to, access_token) => {
               type: "button",
               sub_type: "url",
               index:0,
-              parameters: [],
             },
           ],
         },
